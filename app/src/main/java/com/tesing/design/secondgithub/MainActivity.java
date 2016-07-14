e5e7ac164776cf4599873133633786e3a7c1a002
khaled90mobarak <khaled.mobarak1990@yahoo.com>
2016-07-14 17:14:55 +0200

    this is a first comment

--- app/src/main/java/com/tesing/design/secondgithub/MainActivity.java
+++ app/src/main/java/com/tesing/design/secondgithub/MainActivity.java
@@ -11,6 +11,9 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    // github
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
